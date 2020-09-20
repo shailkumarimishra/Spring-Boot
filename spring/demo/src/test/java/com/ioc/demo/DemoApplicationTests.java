7cd21670668a6021cbc50aa3ae8e46656b8159c8
shailkumarimishra <shailtinu2000@gmail.com>
2020-09-20 20:01:22 +0530

    spring-initializer

---
+++ spring/demo/src/test/java/com/ioc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
